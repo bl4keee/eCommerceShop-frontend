ad09854f6cc96dc19515d097290c5dce3c6abcfb
Bartek <bl4kee@wp.pl>
2020-11-02 15:13:05 +0100

    Removing underline from buttons 'Add to cart' and from category pane

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
   
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -27,9 +28,11 @@ export class ProductListComponent implements OnInit {
 
     if(hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     }
     else {
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
