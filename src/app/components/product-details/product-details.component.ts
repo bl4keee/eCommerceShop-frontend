b9b47ddadb1d7ffe3445afe57b68c4602344e64a
Bartek <bl4kee@wp.pl>
2020-11-05 19:50:27 +0100

    Enhanced product details component to retrieve product service

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  constructor(private ProductService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  handleProductDetails() {
+    const theProductId: number = +this.route.snapshot.paramMap.get('id');
+
+    this.ProductService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
   }
 
 }
