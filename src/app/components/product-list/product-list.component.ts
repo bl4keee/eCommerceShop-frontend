bb3dfe55afc0bcf9d5d4fae2f06d54d14dc79c9a
Bartek <bl4kee@wp.pl>
2020-11-07 19:33:49 +0100

    Update ProductListComponent to handle pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -10,10 +10,14 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
-  currentCategoryId: number;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   currentCategoryName: string;
-  searchMode: boolean;
+  searchMode: boolean = false;
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0; 
   
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -57,10 +61,26 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryName = 'Books';
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+    this.productService.getProductListPaginate(this.thePageNumber - 1, 
+                                                this.thePageSize, 
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());
+  }
+  
+  private processResult() {
+    return data => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
   }
 }
