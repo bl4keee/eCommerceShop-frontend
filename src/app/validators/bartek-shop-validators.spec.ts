8d7289767614250f58b765c0d5f9f3b648425d41
Bartek <bl4kee@wp.pl>
2020-11-24 18:24:27 +0100

    Developed custom validator and defined custom validator rule

---
+++ src/app/validators/bartek-shop-validators.spec.ts
@@ -0,0 +1,7 @@
+import { BartekShopValidators } from './bartek-shop-validators';
+
+describe('BartekShopValidators', () => {
+  it('should create an instance', () => {
+    expect(new BartekShopValidators()).toBeTruthy();
+  });
+});
