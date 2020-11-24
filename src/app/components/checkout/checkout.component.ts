742dead64b882b1c66221bba6a0aed18ef2bddc2
Bartek <bl4kee@wp.pl>
2020-11-24 18:29:46 +0100

    Added custom validation for lastName

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -33,7 +33,8 @@ export class CheckoutComponent implements OnInit {
                                    BartekShopValidators.notOnlyWhitespace]),
         lastName: new FormControl('', 
                                   [Validators.required, 
-                                   Validators.minLength(2)]),
+                                   Validators.minLength(2), 
+                                   BartekShopValidators.notOnlyWhitespace]),
         email: new FormControl('', 
                                   [Validators.required, 
                                    Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
