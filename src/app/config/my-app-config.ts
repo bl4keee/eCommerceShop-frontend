5461e9168321f59aafc0814cb9add5ffa2b6bc96
Bartek <bl4kee@wp.pl>
2021-01-04 22:38:02 +0100

    Created config for okta integration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3byrwrqT63V7fc5d6',
+        issuer: 'https://dev-1083524.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
