a20979adc583fa97fdf038c82e9bf2a007913c12
Bartek <bl4kee@wp.pl>
2021-01-11 12:30:09 +0100

    Updated cartService to read data from session storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,8 +10,17 @@ export class CartService {
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
+  storage: Storage = sessionStorage;
 
-  constructor() { }
+  constructor() {
+
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null) {
+      this.cartItems = data;
+      this.computeCartTotals();
+    }
+   }
 
   addToCart(theCartItem: CartItem) {
     
