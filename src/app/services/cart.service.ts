fb8c97a6ea722b5d318b2f1a9a318fd24f880de2
Bartek <bl4kee@wp.pl>
2021-01-11 12:41:03 +0100

    Changed storage type from session to local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,7 +10,8 @@ export class CartService {
   cartItems: CartItem[] = [];
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
-  storage: Storage = sessionStorage;
+  //storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
 
   constructor() {
 
